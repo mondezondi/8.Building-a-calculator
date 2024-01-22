5a887198f473f0ade01155a39848bfc30c166850
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-22 18:59:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Building_a_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            double num4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num3 + num4);
+            Console.Read();
+        }
+    }
+}
